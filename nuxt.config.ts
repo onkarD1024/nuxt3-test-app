02300dfd12821eb2cd13320d1da7cfa0bc1fd8d1
Onkar Deshpande <onkar@Onkars-Air.lan>
2025-01-04 16:11:56 +0530

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  ssr: false, // Disable SSR if you're deploying as a static app
+});
